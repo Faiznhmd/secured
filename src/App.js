899f40f1572c7a412273f78e5248c7291784ffc1
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-11-15 12:38:45 +0530

    hero section completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Hero from './components/hero/Hero.jsx';
+import Navbar from './components/navbar/Navbar.jsx';
+
 import './App.css';
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Hero />
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
