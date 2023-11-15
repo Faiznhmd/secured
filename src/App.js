d74d7a97a60321297723dfe3d7245842503efeb6
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-11-15 14:29:26 +0530

    data section completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Hero from './components/hero/Hero.jsx';
 import Navbar from './components/navbar/Navbar.jsx';
+import Data from './components/Data/Data.jsx';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Hero />
       <Navbar />
+      <Hero />
+      <Data />
     </div>
   );
 }
---
+++ src/components/Data/Data.jsx
@@ -0,0 +1,26 @@
+import './Data.css';
+
+const Data = () => {
+  return (
+    <div className="data">
+      <div className="container">
+        <div className="content">
+          <h1>
+            <span>Data</span>Recovery
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad
+            expedita, vel tenetur sunt, reprehenderit inventore illo officia
+            omnis dolor odit atque ullam! Perferendis reiciendis impedit minus
+            voluptatum sunt neque? Velit?
+          </p>
+          <div>
+            <button>Sign up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Data;
